524782c7efea1d8e61ab9ddc92fb2f163e71d683
phuocnv1 <vinhphuoc1892@gmail.com>
2025-11-07 15:21:43 +0700

    update gitinogre

--- support.server/DTOs/UserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace support.server.DTOs
-{
-    public class UserDto
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
--- support.server/Models/ResponseData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace support.server.Models
-{
-    public class ApiResponse<T>
-    {
-        public int status { get; set; }
-        public string message { get; set; }
-        public T Data { get; set; }
-    }
-}
